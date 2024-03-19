bd8d042af9d269bfe2622c36f5cbeedb5db467f1
Divya Gambhir <divya.gambhir2003@gmail.com>
2024-03-19 23:37:33 +0530

    change index

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const masterKey = "4VGP2DN-6EWM4SJ-N6FGRHV-Z3PR3TT";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//1. GET a random joke'
+//1. GET a random job'
 
 app.get("/random",(req,res)=>{
   const jobGet = Math.floor(Math.random()*jobs.length);
@@ -97,572 +97,572 @@ app.listen(port, () => {
 var jobs = [
   {
     id: 1,
-    jokeText:
+    jobText:
       "Why don't scientists trust atoms? Because they make up everything.",
-    jokeType: "Science",
+    jobType: "Science",
   },
   {
     id: 2,
-    jokeText:
+    jobText:
       "Why did the scarecrow win an award? Because he was outstanding in his field.",
-    jokeType: "Puns",
+    jobType: "Puns",
   },
   {
     id: 3,
-    jokeText:
+    jobText:
       "I told my wife she was drawing her eyebrows too high. She looked surprised.",
-    jokeType: "Puns",
+    jobType: "Puns",
   },
   {
     id: 4,
-    jokeText:
+    jobText:
       "What did one ocean say to the other ocean? Nothing, they just waved.",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 5,
-    jokeText:
+    jobText:
       "Why do we never tell secrets on a farm? Because the potatoes have eyes and the corn has ears.",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 6,
-    jokeText: "How do you organize a space party? You planet!",
-    jokeType: "Science",
+    jobText: "How do you organize a space party? You planet!",
+    jobType: "Science",
   },
   {
     id: 7,
-    jokeText:
+    jobText:
       "Why don't some couples go to the gym? Because some relationships don't work out.",
-    jokeType: "Puns",
+    jobType: "Puns",
   },
   {
     id: 8,
-    jokeText:
+    jobText:
       "Parallel lines have so much in common. It's a shame they'll never meet.",
-    jokeType: "Math",
+    jobType: "Math",
   },
   {
     id: 9,
-    jokeText: "What do you call fake spaghetti? An impasta!",
-    jokeType: "Food",
+    jobText: "What do you call fake spaghetti? An impasta!",
+    jobType: "Food",
   },
   {
     id: 10,
-    jokeText: "Why did the tomato turn red? Because it saw the salad dressing!",
-    jokeType: "Food",
+    jobText: "Why did the tomato turn red? Because it saw the salad dressing!",
+    jobType: "Food",
   },
   {
     id: 11,
-    jokeText:
+    jobText:
       "What do you get when you cross a snowman and a vampire? Frostbite!",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 12,
-    jokeText:
+    jobText:
       "Why did the golfer bring two pairs of pants? In case he got a hole in one!",
-    jokeType: "Sports",
+    jobType: "Sports",
   },
   {
     id: 13,
-    jokeText:
+    jobText:
       "Why are ghosts bad at lying? Because you can see right through them!",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 14,
-    jokeText: "Why can't you give Elsa a balloon? Because she will let it go.",
-    jokeType: "Movies",
+    jobText: "Why can't you give Elsa a balloon? Because she will let it go.",
+    jobType: "Movies",
   },
   {
     id: 15,
-    jokeText:
+    jobText:
       "I'm reading a book about anti-gravity. It's impossible to put down!",
-    jokeType: "Science",
+    jobType: "Science",
   },
   {
     id: 16,
-    jokeText:
+    jobText:
       "I told my wife she was drawing her eyebrows too high. She looked surprised.",
-    jokeType: "Puns",
+    jobType: "Puns",
   },
   {
     id: 17,
-    jokeText:
+    jobText:
       "What did one ocean say to the other ocean? Nothing, they just waved.",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 18,
-    jokeText:
+    jobText:
       "Why do we never tell secrets on a farm? Because the potatoes have eyes and the corn has ears.",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 19,
-    jokeText: "How do you organize a space party? You planet!",
-    jokeType: "Science",
+    jobText: "How do you organize a space party? You planet!",
+    jobType: "Science",
   },
   {
     id: 20,
-    jokeText:
+    jobText:
       "Why don't some couples go to the gym? Because some relationships don't work out.",
-    jokeType: "Puns",
+    jobType: "Puns",
   },
   {
     id: 21,
-    jokeText:
+    jobText:
       "Parallel lines have so much in common. It's a shame they'll never meet.",
-    jokeType: "Math",
+    jobType: "Math",
   },
   {
     id: 22,
-    jokeText: "What do you call fake spaghetti? An impasta!",
-    jokeType: "Food",
+    jobText: "What do you call fake spaghetti? An impasta!",
+    jobType: "Food",
   },
   {
     id: 23,
-    jokeText: "Why did the tomato turn red? Because it saw the salad dressing!",
-    jokeType: "Food",
+    jobText: "Why did the tomato turn red? Because it saw the salad dressing!",
+    jobType: "Food",
   },
   {
     id: 24,
-    jokeText:
+    jobText:
       "What do you get when you cross a snowman and a vampire? Frostbite!",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 25,
-    jokeText:
+    jobText:
       "Why did the golfer bring two pairs of pants? In case he got a hole in one!",
-    jokeType: "Sports",
+    jobType: "Sports",
   },
   {
     id: 26,
-    jokeText:
+    jobText:
       "Why are ghosts bad at lying? Because you can see right through them!",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 27,
-    jokeText: "Why can't you give Elsa a balloon? Because she will let it go.",
-    jokeType: "Movies",
+    jobText: "Why can't you give Elsa a balloon? Because she will let it go.",
+    jobType: "Movies",
   },
   {
     id: 28,
-    jokeText:
+    jobText:
       "I'm reading a book about anti-gravity. It's impossible to put down!",
-    jokeType: "Science",
+    jobType: "Science",
   },
   {
     id: 29,
-    jokeText:
+    jobText:
       "I told my wife she was drawing her eyebrows too high. She looked surprised.",
-    jokeType: "Puns",
+    jobType: "Puns",
   },
   {
     id: 30,
-    jokeText:
+    jobText:
       "What did one ocean say to the other ocean? Nothing, they just waved.",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 31,
-    jokeText:
+    jobText:
       "Why do we never tell secrets on a farm? Because the potatoes have eyes and the corn has ears.",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 32,
-    jokeText: "How do you organize a space party? You planet!",
-    jokeType: "Science",
+    jobText: "How do you organize a space party? You planet!",
+    jobType: "Science",
   },
   {
     id: 33,
-    jokeText:
+    jobText:
       "Why don't some couples go to the gym? Because some relationships don't work out.",
-    jokeType: "Puns",
+    jobType: "Puns",
   },
   {
     id: 34,
-    jokeText:
+    jobText:
       "Parallel lines have so much in common. It's a shame they'll never meet.",
-    jokeType: "Math",
+    jobType: "Math",
   },
   {
     id: 35,
-    jokeText: "What do you call fake spaghetti? An impasta!",
-    jokeType: "Food",
+    jobText: "What do you call fake spaghetti? An impasta!",
+    jobType: "Food",
   },
   {
     id: 36,
-    jokeText: "Why did the tomato turn red? Because it saw the salad dressing!",
-    jokeType: "Food",
+    jobText: "Why did the tomato turn red? Because it saw the salad dressing!",
+    jobType: "Food",
   },
   {
     id: 37,
-    jokeText:
+    jobText:
       "What do you get when you cross a snowman and a vampire? Frostbite!",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 38,
-    jokeText:
+    jobText:
       "Why did the golfer bring two pairs of pants? In case he got a hole in one!",
-    jokeType: "Sports",
+    jobType: "Sports",
   },
   {
     id: 39,
-    jokeText:
+    jobText:
       "Why are ghosts bad at lying? Because you can see right through them!",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 40,
-    jokeText: "Why can't you give Elsa a balloon? Because she will let it go.",
-    jokeType: "Movies",
+    jobText: "Why can't you give Elsa a balloon? Because she will let it go.",
+    jobType: "Movies",
   },
   {
     id: 41,
-    jokeText:
+    jobText:
       "I'm reading a book about anti-gravity. It's impossible to put down!",
-    jokeType: "Science",
+    jobType: "Science",
   },
   {
     id: 42,
-    jokeText:
+    jobText:
       "I told my wife she was drawing her eyebrows too high. She looked surprised.",
-    jokeType: "Puns",
+    jobType: "Puns",
   },
   {
     id: 43,
-    jokeText:
+    jobText:
       "What did one ocean say to the other ocean? Nothing, they just waved.",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 44,
-    jokeText:
+    jobText:
       "Why do we never tell secrets on a farm? Because the potatoes have eyes and the corn has ears.",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 45,
-    jokeText: "How do you organize a space party? You planet!",
-    jokeType: "Science",
+    jobText: "How do you organize a space party? You planet!",
+    jobType: "Science",
   },
   {
     id: 46,
-    jokeText:
+    jobText:
       "Why don't some couples go to the gym? Because some relationships don't work out.",
-    jokeType: "Puns",
+    jobType: "Puns",
   },
   {
     id: 47,
-    jokeText:
+    jobText:
       "Parallel lines have so much in common. It's a shame they'll never meet.",
-    jokeType: "Math",
+    jobType: "Math",
   },
   {
     id: 48,
-    jokeText: "What do you call fake spaghetti? An impasta!",
-    jokeType: "Food",
+    jobText: "What do you call fake spaghetti? An impasta!",
+    jobType: "Food",
   },
   {
     id: 49,
-    jokeText: "Why did the tomato turn red? Because it saw the salad dressing!",
-    jokeType: "Food",
+    jobText: "Why did the tomato turn red? Because it saw the salad dressing!",
+    jobType: "Food",
   },
   {
     id: 50,
-    jokeText:
+    jobText:
       "What do you get when you cross a snowman and a vampire? Frostbite!",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 51,
-    jokeText:
+    jobText:
       "Why did the golfer bring two pairs of pants? In case he got a hole in one!",
-    jokeType: "Sports",
+    jobType: "Sports",
   },
   {
     id: 52,
-    jokeText:
+    jobText:
       "Why are ghosts bad at lying? Because you can see right through them!",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 53,
-    jokeText: "Why can't you give Elsa a balloon? Because she will let it go.",
-    jokeType: "Movies",
+    jobText: "Why can't you give Elsa a balloon? Because she will let it go.",
+    jobType: "Movies",
   },
   {
     id: 54,
-    jokeText:
+    jobText:
       "I'm reading a book about anti-gravity. It's impossible to put down!",
-    jokeType: "Science",
+    jobType: "Science",
   },
   {
     id: 55,
-    jokeText:
+    jobText:
       "I told my wife she was drawing her eyebrows too high. She looked surprised.",
-    jokeType: "Puns",
+    jobType: "Puns",
   },
   {
     id: 56,
-    jokeText:
+    jobText:
       "What did one ocean say to the other ocean? Nothing, they just waved.",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 57,
-    jokeText:
+    jobText:
       "Why do we never tell secrets on a farm? Because the potatoes have eyes and the corn has ears.",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 58,
-    jokeText: "How do you organize a space party? You planet!",
-    jokeType: "Science",
+    jobText: "How do you organize a space party? You planet!",
+    jobType: "Science",
   },
   {
     id: 59,
-    jokeText:
+    jobText:
       "Why don't some couples go to the gym? Because some relationships don't work out.",
-    jokeType: "Puns",
+    jobType: "Puns",
   },
   {
     id: 60,
-    jokeText:
+    jobText:
       "Parallel lines have so much in common. It's a shame they'll never meet.",
-    jokeType: "Math",
+    jobType: "Math",
   },
   {
     id: 61,
-    jokeText: "What do you call fake spaghetti? An impasta!",
-    jokeType: "Food",
+    jobText: "What do you call fake spaghetti? An impasta!",
+    jobType: "Food",
   },
   {
     id: 62,
-    jokeText: "Why did the tomato turn red? Because it saw the salad dressing!",
-    jokeType: "Food",
+    jobText: "Why did the tomato turn red? Because it saw the salad dressing!",
+    jobType: "Food",
   },
   {
     id: 63,
-    jokeText:
+    jobText:
       "What do you get when you cross a snowman and a vampire? Frostbite!",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 64,
-    jokeText:
+    jobText:
       "Why did the golfer bring two pairs of pants? In case he got a hole in one!",
-    jokeType: "Sports",
+    jobType: "Sports",
   },
   {
     id: 65,
-    jokeText:
+    jobText:
       "Why are ghosts bad at lying? Because you can see right through them!",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 66,
-    jokeText: "Why can't you give Elsa a balloon? Because she will let it go.",
-    jokeType: "Movies",
+    jobText: "Why can't you give Elsa a balloon? Because she will let it go.",
+    jobType: "Movies",
   },
   {
     id: 67,
-    jokeText:
+    jobText:
       "I'm reading a book about anti-gravity. It's impossible to put down!",
-    jokeType: "Science",
+    jobType: "Science",
   },
   {
     id: 68,
-    jokeText:
+    jobText:
       "I told my wife she was drawing her eyebrows too high. She looked surprised.",
-    jokeType: "Puns",
+    jobType: "Puns",
   },
   {
     id: 69,
-    jokeText:
+    jobText:
       "What did one ocean say to the other ocean? Nothing, they just waved.",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 70,
-    jokeText:
+    jobText:
       "Why do we never tell secrets on a farm? Because the potatoes have eyes and the corn has ears.",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 71,
-    jokeText: "How do you organize a space party? You planet!",
-    jokeType: "Science",
+    jobText: "How do you organize a space party? You planet!",
+    jobType: "Science",
   },
   {
     id: 72,
-    jokeText:
+    jobText:
       "Why don't some couples go to the gym? Because some relationships don't work out.",
-    jokeType: "Puns",
+    jobType: "Puns",
   },
   {
     id: 73,
-    jokeText:
+    jobText:
       "Parallel lines have so much in common. It's a shame they'll never meet.",
-    jokeType: "Math",
+    jobType: "Math",
   },
   {
     id: 74,
-    jokeText: "What do you call fake spaghetti? An impasta!",
-    jokeType: "Food",
+    jobText: "What do you call fake spaghetti? An impasta!",
+    jobType: "Food",
   },
   {
     id: 75,
-    jokeText: "Why did the tomato turn red? Because it saw the salad dressing!",
-    jokeType: "Food",
+    jobText: "Why did the tomato turn red? Because it saw the salad dressing!",
+    jobType: "Food",
   },
   {
     id: 76,
-    jokeText:
+    jobText:
       "What do you get when you cross a snowman and a vampire? Frostbite!",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 77,
-    jokeText:
+    jobText:
       "Why did the golfer bring two pairs of pants? In case he got a hole in one!",
-    jokeType: "Sports",
+    jobType: "Sports",
   },
   {
     id: 78,
-    jokeText:
+    jobText:
       "Why are ghosts bad at lying? Because you can see right through them!",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 79,
-    jokeText: "Why can't you give Elsa a balloon? Because she will let it go.",
-    jokeType: "Movies",
+    jobText: "Why can't you give Elsa a balloon? Because she will let it go.",
+    jobType: "Movies",
   },
   {
     id: 80,
-    jokeText:
+    jobText:
       "I'm reading a book about anti-gravity. It's impossible to put down!",
-    jokeType: "Science",
+    jobType: "Science",
   },
   {
     id: 81,
-    jokeText:
+    jobText:
       "I told my wife she was drawing her eyebrows too high. She looked surprised.",
-    jokeType: "Puns",
+    jobType: "Puns",
   },
   {
     id: 82,
-    jokeText:
+    jobText:
       "What did one ocean say to the other ocean? Nothing, they just waved.",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 83,
-    jokeText:
+    jobText:
       "Why do we never tell secrets on a farm? Because the potatoes have eyes and the corn has ears.",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 84,
-    jokeText: "How do you organize a space party? You planet!",
-    jokeType: "Science",
+    jobText: "How do you organize a space party? You planet!",
+    jobType: "Science",
   },
   {
     id: 85,
-    jokeText:
+    jobText:
       "Why don't some couples go to the gym? Because some relationships don't work out.",
-    jokeType: "Puns",
+    jobType: "Puns",
   },
   {
     id: 86,
-    jokeText:
+    jobText:
       "Parallel lines have so much in common. It's a shame they'll never meet.",
-    jokeType: "Math",
+    jobType: "Math",
   },
   {
     id: 87,
-    jokeText: "What do you call fake spaghetti? An impasta!",
-    jokeType: "Food",
+    jobText: "What do you call fake spaghetti? An impasta!",
+    jobType: "Food",
   },
   {
     id: 88,
-    jokeText: "Why did the tomato turn red? Because it saw the salad dressing!",
-    jokeType: "Food",
+    jobText: "Why did the tomato turn red? Because it saw the salad dressing!",
+    jobType: "Food",
   },
   {
     id: 89,
-    jokeText:
+    jobText:
       "What do you get when you cross a snowman and a vampire? Frostbite!",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 90,
-    jokeText:
+    jobText:
       "Why did the golfer bring two pairs of pants? In case he got a hole in one!",
-    jokeType: "Sports",
+    jobType: "Sports",
   },
   {
     id: 91,
-    jokeText:
+    jobText:
       "Why are ghosts bad at lying? Because you can see right through them!",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 92,
-    jokeText: "Why can't you give Elsa a balloon? Because she will let it go.",
-    jokeType: "Movies",
+    jobText: "Why can't you give Elsa a balloon? Because she will let it go.",
+    jobType: "Movies",
   },
   {
     id: 93,
-    jokeText:
+    jobText:
       "I'm reading a book about anti-gravity. It's impossible to put down!",
-    jokeType: "Science",
+    jobType: "Science",
   },
   {
     id: 94,
-    jokeText:
+    jobText:
       "I told my wife she was drawing her eyebrows too high. She looked surprised.",
-    jokeType: "Puns",
+    jobType: "Puns",
   },
   {
     id: 95,
-    jokeText:
+    jobText:
       "What did one ocean say to the other ocean? Nothing, they just waved.",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 96,
-    jokeText:
+    jobText:
       "Why do we never tell secrets on a farm? Because the potatoes have eyes and the corn has ears.",
-    jokeType: "Wordplay",
+    jobType: "Wordplay",
   },
   {
     id: 97,
-    jokeText: "How do you organize a space party? You planet!",
-    jokeType: "Science",
+    jobText: "How do you organize a space party? You planet!",
+    jobType: "Science",
   },
   {
     id: 98,
-    jokeText:
+    jobText:
       "Why don't some couples go to the gym? Because some relationships don't work out.",
-    jokeType: "Puns",
+    jobType: "Puns",
   },
   {
     id: 99,
-    jokeText:
+    jobText:
       "Parallel lines have so much in common. It's a shame they'll never meet.",
-    jokeType: "Math",
+    jobType: "Math",
   },
   {
     id: 100,
-    jokeText: "What do you call fake spaghetti? An impasta!",
-    jokeType: "Food",
+    jobText: "What do you call fake spaghetti? An impasta!",
+    jobType: "Food",
   },
 ];
